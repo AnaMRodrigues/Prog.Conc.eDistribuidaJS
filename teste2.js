54b8ed4bb4732d4d36abb19471a026eda4a7139c
AnaMRodrigues <88545978+AnaMRodrigues@users.noreply.github.com>
2022-06-03 12:09:41 -0300

    Add files via upload

--- teste2.js
+++ teste2.js
@@ -2,8 +2,4 @@ const fs = require('fs');
 const path = require('path');
 const coolPath = path.join(__dirname, 'empresas_bahia.csv');
 const stream = fs.createReadStream(coolPath);
-// Create readable stream
-//var readableStream = fs.createReadStream('empresas_bahia.csv');
-
-// Pipe it to out stdout
 stream.pipe(process.stdout);
